9cc1c3a2902bde15460ac9d93c635890f2519bcd
Stanley <leopck@users.noreply.github.com>
2016-03-02 17:26:30 +0800

    Update query.js

--- query.js
+++ query.js
@@ -6,7 +6,17 @@ $(document).ready(function() {
       type: "GET",
       url: "https://www.google.com/finance/converter", // it's the URL of your component
       data: { a:"1", from:"MYR" , to:"CNY" }, // serializes the form's elements
-      headers: {"Access-Control-Allow-Origin": "http://leopck.github.io/"},
+      headers: {
+         ":authority": "www.google.com",
+         ":method": "GET",
+         ":path": "/finance/converter?a=1&from=MYR&to=CNY",
+         ":scheme": "https",
+         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+         "accept-encoding": "gzip, deflate, sdch",
+         "accept-language": "en-GB,en-US;q=0.8,en;q=0.6",
+         "upgrade-insecure-requests": "1",
+         "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36",
+      },
       success: function(data)
       {
         // show the data you got from B in result div
