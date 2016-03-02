344a3545540fa24f8e506fd403a96406c40c8e84
Stanley <leopck@users.noreply.github.com>
2016-03-02 17:07:16 +0800

    Update query.js

--- query.js
+++ query.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $.ajax({ // make an AJAX request
       type: "GET",
       url: "http://cors.io/?u=https://www.google.com/finance/converter?a=1&from=MYR&to=CNY", // it's the URL of your component B
-      //data: $("#myform").serialize(), // serializes the form's elements
+      data: { a:"1", from:"MYR" , to:"CNY" }, // serializes the form's elements
       success: function(data)
       {
         // show the data you got from B in result div
