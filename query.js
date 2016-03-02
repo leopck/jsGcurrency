58cedcc92e76ac210a4d0ee6c0d06a2cdb14148c
Stanley <leopck@users.noreply.github.com>
2016-03-02 18:46:26 +0800

    Update query.js

--- query.js
+++ query.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
          "accept-encoding": "gzip, deflate, sdch",
          "accept-language": "en-GB,en-US;q=0.8,en;q=0.6",
+         "origin": "https://leopck.github.io/",
          "upgrade-insecure-requests": "1",
          "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36",
       },
